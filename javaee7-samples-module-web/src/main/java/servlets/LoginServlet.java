06d9f797b2da84a05429504547d776fc7b6747c9
vikych <viky.ch@mail.ru>
2017-12-30 17:34:45 +0200

    PopUpStuff.patch

--- javaee7-samples-module-web/src/main/java/servlets/LoginServlet.java
+++ javaee7-samples-module-web/src/main/java/servlets/LoginServlet.java
@@ -4,18 +4,22 @@ import common.AuthenticationHelper;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/home")
 public class LoginServlet extends HttpServlet {
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String uname = request.getParameter("username");
         String pword = request.getParameter("password");
         response.setContentType("text/html");
 
         if (AuthenticationHelper.isAllowed(uname, pword)) {
+            response.getWriter().write(uname);
             RequestDispatcher dispatcher = request.getRequestDispatcher("app");
             dispatcher.forward(request, response);
         } else {
